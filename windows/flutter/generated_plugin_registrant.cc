6bdbd05a261932df6de8fd578d995eb954631a61
Muhammad Javed <91019922+javedmughal-058@users.noreply.github.com>
2023-01-14 12:30:01 +0500

    Update generated_plugin_registrant.cc

--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -6,9 +6,12 @@
 
 #include "generated_plugin_registrant.h"
 
+#include <dialogflow_grpc/dialogflow_grpc_plugin.h>
 #include <flutter_tts/flutter_tts_plugin.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
+  DialogflowGrpcPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("DialogflowGrpcPlugin"));
   FlutterTtsPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("FlutterTtsPlugin"));
 }
