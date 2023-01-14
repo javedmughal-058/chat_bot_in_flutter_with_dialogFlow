bed085565fb1a015c052583277a18d784070a999
Muhammad Javed <91019922+javedmughal-058@users.noreply.github.com>
2023-01-14 12:29:53 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,144 +1,59 @@
-import 'package:bubble/bubble.dart';
-import 'package:dialogflow_flutter/dialogflowFlutter.dart';
-import 'package:dialogflow_flutter/googleAuth.dart';
+
 import 'package:flutter/material.dart';
-import 'package:speech_to_text/speech_to_text.dart' as stt;
+
+import 'chat.dart';
+
 
 void main() {
-  runApp(const MaterialApp(
-    title: 'US App',
-    debugShowCheckedModeBanner: false,
-    home:  ChatBot(),
-  ));
+  runApp(const MyApp());
 }
 
-class ChatBot extends StatefulWidget {
-  const ChatBot({Key? key}) : super(key: key);
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
-  @override
-  _ChatBotState createState() =>  _ChatBotState();
-}
-
-class _ChatBotState extends State<ChatBot> {
-
-  final messageInsert = TextEditingController();
-  List<Map> messsages = [];
-
-  void response(query) async {
-    AuthGoogle authGoogle = await AuthGoogle(fileJson: "assets/farmtech-fh9j-3db6e0409c71.json").build();
-    DialogFlow dialogflow = DialogFlow(authGoogle: authGoogle, language: "en");
-    AIResponse aiResponse = await dialogflow.detectIntent(query);
-    setState(() {
-      messsages.insert(0, {
-        "data": 0,
-        "message": aiResponse.getListMessage()![0]["text"]["text"][0].toString()
-      });
-    });
-  }
-
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          toolbarHeight: 70,
-          shape: const RoundedRectangleBorder(
-            borderRadius: BorderRadius.only(
-              bottomLeft:  Radius.circular(30),
-              bottomRight: Radius.circular(30),
-            ),
-          ),
-          elevation: 10,
-          title: const Text("DialogFlow Chat_bot"),
-        ),
-        body: Column(
-          children: [
-            Flexible(
-                child: ListView.builder(
-                    reverse: true,
-                    itemCount: messsages.length,
-                    itemBuilder: (context, index) => chat(
-                        messsages[index]["message"].toString(),
-                        messsages[index]["data"]))),
-            const Divider(
-              height: 6.0,
-            ),
-            Container(
-              padding: const EdgeInsets.only(left: 15.0, right: 15.0, bottom: 20),
-              margin: const EdgeInsets.symmetric(horizontal: 8.0),
-              child: Row(
-                children: <Widget>[
-                  Flexible(
-                      child: TextField(
-                        controller: messageInsert,
-                        decoration: const InputDecoration.collapsed(
-                            hintText: "Send your message",
-                            hintStyle: TextStyle(
-                                fontWeight: FontWeight.bold, fontSize: 18.0)),
-                      )),
-                  Container(
-                    margin: const EdgeInsets.symmetric(horizontal: 4.0),
-                    child: IconButton(
-                        icon: const Icon(
-                          Icons.send,
-                          size: 30.0,
-                        ),
-                        onPressed: () {
-                          if (messageInsert.text.isEmpty) {
-                            print("empty message");
-                          } else {
-                            setState(() {
-                              messsages.insert(0,
-                                  {"data": 1, "message": messageInsert.text});
-                            });
-                            response(messageInsert.text);
-                            messageInsert.clear();
-                          }
-                        }),
-                  )
-                ],
-              ),
-            ),
-            const SizedBox(
-              height: 15.0,
-            )
-          ],
-        ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.orange,
       ),
+      home: const MyHomePage(title: 'Flutter Dialogflow Agent'),
     );
   }
+}
 
-  Widget chat(String message, int data) {
-    return Padding(
-      padding: const EdgeInsets.all(10.0),
-      child: Bubble(
-          radius: const Radius.circular(15.0),
-          color: data == 0 ? Colors.blue : Colors.orangeAccent,
-          elevation: 0.0,
-          alignment: data == 0 ? Alignment.topLeft : Alignment.topRight,
-          nip: data == 0 ? BubbleNip.leftBottom : BubbleNip.rightTop,
-          child: Padding(
-            padding: const EdgeInsets.all(2.0),
-            child: Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                CircleAvatar(
-                  backgroundImage: AssetImage(
-                      data == 0 ? "assets/images/bot.png" : "assets/images/user.jpeg"),
-                ),
-                const SizedBox(
-                  width: 10.0,
-                ),
-                Flexible(
-                    child: Text(
-                      message,
-                      style: const TextStyle(
-                          color: Colors.white, fontWeight: FontWeight.bold),
-                    ))
-              ],
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+
+    return SafeArea(
+      child: Scaffold(
+          appBar: AppBar(
+            centerTitle: true,
+            toolbarHeight: 70,
+            shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.only(
+                bottomLeft:  Radius.circular(30),
+                bottomRight: Radius.circular(30),
+              ),
             ),
-          )),
+            elevation: 10,
+            title: Text(widget.title, style: const TextStyle(color: Colors.white)),
+          ),
+          body: const Center(
+              child: Chat())
+      ),
     );
   }
 }
